4191fc84b45a9c52f40e332953cb0dfefabad7e1
Erik Härkönen <erik.harkonen@hotmail.com>
2018-02-20 16:53:06 +0100

    Prefer dedicated GPU

--- Gamma/Sources/main.cpp
+++ Gamma/Sources/main.cpp
@@ -1,6 +1,10 @@
 #include "GammaCore.hpp"
 #include <cstdlib>
 
+extern "C" {
+    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
+
 int main(int argc, char * argv[]) {
     GammaCore core;
     core.mainLoop();
